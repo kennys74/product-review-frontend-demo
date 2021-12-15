d4bdd2307b88ec2fb1cc07ea9e659458d32e868b
Kenneth Sutherland <ken@completewebsolutions.co.za>
2021-12-15 10:23:08 +0200

    Update HomeDefaultTopCategories.jsx
    Added a few extra category blocks to fill the home page

--- components/partials/homepage/home-default/HomeDefaultTopCategories.jsx
+++ components/partials/homepage/home-default/HomeDefaultTopCategories.jsx
@@ -6,16 +6,40 @@ const HomeDefaultTopCategories = () => (
         <div className="ps-container">
             <h3>Shop by Category</h3>
             <div className="row">
-				<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 ">
+                <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6 ">
                     <div className="ps-block--category">
-                        <Link href="/category/air-fryers-1">
-                            <a className="ps-block__overlay"></a>
-                        </Link>
-                        <img
-                            src="/static/img/categories/air-fryers.jpg"
-                            alt="Air Fryers"
-                        />
-                        <p>Air Fryers</p>
+                            <Link href="/category/air-fryers-1">
+                                <a className="ps-block__overlay"></a>
+                            </Link>
+                            <img
+                                src="/static/img/categories/air-fryers.jpg"
+                                alt="Air Fryers"
+                            />
+                            <p>Air Fryers</p>
+                    </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                            <Link href="/category/air-fryers-1">
+                                <a className="ps-block__overlay"></a>
+                            </Link>
+                            <img
+                                src="/static/img/categories/air-fryers.jpg"
+                                alt="Air Fryers"
+                            />
+                            <p>Air Fryers</p>
+                    </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-6 ">
+                    <div className="ps-block--category">
+                            <Link href="/category/air-fryers-1">
+                                <a className="ps-block__overlay"></a>
+                            </Link>
+                            <img
+                                src="/static/img/categories/air-fryers.jpg"
+                                alt="Air Fryers"
+                            />
+                            <p>Air Fryers</p>
                     </div>
                 </div>
             </div>
